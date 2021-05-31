5ccced3f75c1b07199c19463184075c2a472d681
80010-13-04 <gourlain.david2@gmail.com>
2021-05-31 16:55:30 +0200

    js part1.5

--- FRONT/DYNAMIQUE/JAVASCRIPT/ex1.js
+++ FRONT/DYNAMIQUE/JAVASCRIPT/ex1.js
@@ -1,5 +1,19 @@
-function.clickbtn1()
+var bouton1= document.getElementById("relance");
+bouton1.addEventListener("click",clickbtn1);
+
+function clickbtn1()
 {
-    var nom = prompt("Entrez votre nom")
-    
+    var nom = prompt("Entrez votre nom");
+
+    var prenom = prompt("Entrez votre prénom");
+
+    var reponse = confirm("Etes-vous un homme?");
+    if (reponse == true) 
+    {
+        alert("Bonjour Monsieur "+nom +prenom);
+    }/* 
+    else 
+    {
+        alert("Bonjour Madame" +nom+prenom+/n/n"Bienvenue sur notre site web !");
+    }*/
 }
