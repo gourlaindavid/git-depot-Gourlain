1893ca8960e0863f2a82e55547a4bb65dc4c9db3
80010-13-04 <gourlain.david2@gmail.com>
2021-06-10 16:00:03 +0200

    js2 part3

--- FRONT/DYNAMIQUE/JAVASCRIPT/part2/ex.js
+++ FRONT/DYNAMIQUE/JAVASCRIPT/part2/ex.js
@@ -1,21 +1,39 @@
-var tab= new Array();
-var condi;
-var i= 0;
-console.log("ok");
-while(condi!=0)
+//exercice 1
+var bouton1= document.getElementById("ex1");
+bouton1.addEventListener("click",clickbtn1);
+
+function clickbtn1()
 {
-    parseInt(condi=window.prompt("Entrez un nombre (Entrez 0 pour terminer les entrer)"));
-    if(condi!=0)
+    var tab= new Array();
+    var condi;
+    var i= 0;
+    console.log("ok");
+    while(condi!=0)
     {
-        tab[i]=condi;
-        i++;
+        parseInt(condi=window.prompt("Entrez un nombre (Entrez 0 pour terminer les entrer)"));
+        if(condi!=0)
+        {
+            tab[i]=parseInt(condi);
+            i++;
+        }
     }
+    console.log("Il y a "+tab.length+" éléments déclarer")
+    var calcul=0;
+    var moy;
+    for(var e=0; e<tab.length; e++)
+    {
+        calcul=calcul+tab[e];
+        console.log(calcul);
+    }
+    moy=calcul/tab.length;
+    console.log(moy);
 }
-console.log("Il y a "+tab.length+" élément rentrer")
-var calcul=parseInt(0);
-var moy;
-for(var e=0; e<tab.length; e++)
+
+//exercice 2
+var bouton2= document.getElementById("ex2");
+bouton2.addEventListener("click",clickbtn2);
+
+function clickbtn2()
 {
-    calcul=calcul+tab[e];
     
 }
---
+++ FRONT/DYNAMIQUE/JAVASCRIPT/part2/integr.js
@@ -0,0 +1,4 @@
+alert("<p>Coucou !</p>");
+console.log("Ah que Coucou !");
+document.write("Coucou beuh !");
+document.write("<h1>Mon 1er script</h1>")
