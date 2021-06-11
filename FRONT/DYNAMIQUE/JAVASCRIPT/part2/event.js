145bbcaaa5268feeb5c66de8335873f153bca1c4
80010-13-04 <gourlain.david2@gmail.com>
2021-06-11 12:12:23 +0200

    js2 part4.5

---
+++ FRONT/DYNAMIQUE/JAVASCRIPT/part2/event.js
@@ -0,0 +1,41 @@
+//ex 1 retour de donner
+var control= document.getElementById("control");
+control.addEventListener("click",clickcontrol);
+function clickcontrol()
+{
+    var donner= document.getElementById("entrer").value;
+    alert("Vous avez saisi: '"+donner+"'");
+}
+
+//ex2 nb magic
+
+var btnverif= document.getElementById("btnverif");
+btnverif.addEventListener("click",clickverif);
+function clickverif()
+{
+    var nbmag=Math.random();
+    console.log(nbmag);
+    var test=false;
+    /*while(test!=true)
+    {
+        console.log(propo);
+        console.log(test);
+        var propo= document.getElementById("verif").value;
+        if(propo==nbmag)
+        {
+            test=true;
+            alert("Bien jouer");
+            break;
+        }
+        if(propo<nbmag)
+        {
+            alert("trop petit");
+            propo=window.prompt("Entrez votre proposition");
+        }
+        if(propo>nbmag)
+        {
+            alert("trop grand");
+            propo=window.prompt("Entrez votre proposition");
+        }
+    }*/
+}
