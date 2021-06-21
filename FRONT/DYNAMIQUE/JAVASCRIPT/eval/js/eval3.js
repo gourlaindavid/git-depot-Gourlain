9a761a7c8c9f0dcb64b6ca91fc0e2a32470b9d92
80010-13-04 <gourlain.david2@gmail.com>
2021-06-21 08:55:00 +0200

    jseval part5

--- FRONT/DYNAMIQUE/JAVASCRIPT/eval/js/eval3.js
+++ FRONT/DYNAMIQUE/JAVASCRIPT/eval/js/eval3.js
@@ -13,16 +13,13 @@ function clickbtn3()
     while(tab[0]!="")
     {
         var nom=prompt("Donnez un nom");
-        if(nom!=tab)
+        var pos= tab.indexOf(nom);
+        if(pos==-1)
         {
-            alert("nom non reconnu");
+            alert("Nom non reconnu");
         }
-        else
-        {
-            var pos= tab.indexOf(nom);
-            var removed= tab.splice(pos,1);
-            tab.push("");
-        }
+        var removed= tab.splice(pos,1);
+        tab.push("");
         document.getElementById("resultat3").innerHTML=tab[0]+", "+tab[1]+", "+tab[2]+", "+tab[3]+", "+tab[4]+", "+tab[5]+", "+tab[6]+", "+tab[7]+", "+tab[8]+", "+tab[9]
         for (var i = 0; i <tab.length; i++) 
         {
